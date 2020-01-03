d831ca8cc670e79c75cbf612590d2a4a1c931511
tenmei matsuo <m.ten.m.ten@gmail.com>
2020-01-03 10:02:29 +0900

    issues#3 投稿一覧スマホレイアウト修正

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,18 +2,18 @@
 
 @section('content')
 
-<div class="py-2">
+<div class="sm:py-2">
   <ul class="last-border-none">
     @foreach ( $posts as $id => $post )
-      <li class="border-b border-gray-300 py-2">
-        <a href="/{{ $post->id }}" class="block flex items-start md:px-4 px-1 py-2">
+      <li class="border-b border-gray-300 sm:py-2">
+        <a href="/{{ $post->id }}" class="block flex items-start sm:px-4 py-2">
           @if (isset($post->post_thumbnail))
-            <img class="md:mr-8 mr-2 sm:w-48 w-40" src="/thumbnail/{{ $post->post_thumbnail }}" alt="">
+            <img class="md:mr-8 mr-2 sm:w-48 w-24" src="/thumbnail/{{ $post->post_thumbnail }}" alt="">
           @endif
           <div>
-            <span class="text-gray-600">{{ $post->post_published->format('Y.m.d') }}</span>
-            <h2 class="text-lg sm:text-2xl mb-2">{{ $post->post_title }}</h2>
-            <p class="text-gray-700">{{ $post->post_excerpt }}</p>
+            <span class="text-gray-600 text-xs">{{ $post->post_published->format('Y.m.d') }}</span>
+            <h2 class="sm:text-2xl leading-tight sm:mb-2">{{ $post->post_title }}</h2>
+            <p class="text-gray-700 hidden sm:block">{{ $post->post_excerpt }}</p>
           </div>
         </a>
       </li>
