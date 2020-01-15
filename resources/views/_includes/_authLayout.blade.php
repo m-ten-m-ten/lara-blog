1c131deebae25452f94df6af3b75d37abcc390d4
tenmei matsuo <m.ten.m.ten@gmail.com>
2020-01-15 17:51:04 +0900

    カテゴリー・タグの記事数表示追加

--- resources/views/_includes/_authLayout.blade.php
+++ resources/views/_includes/_authLayout.blade.php
@@ -1,12 +1,12 @@
-@extends('_includes._adminLayout')
+@extends('_includes._dashboardLayout')
 @section('content')
 
-<div class="flex items-center justify-center  my-4">
+<div class="flex items-center justify-center my-4">
   <div class="md:w-1/3 sm:w-1/2 w-10/12">
-    <h1 class="text-center text-xl font-bold py-2">@yield('title')</h1>
+    <h1 class="text-center text-xl font-bold py-2 mb-4">@yield('title')</h1>
 
     @if (session('status'))
-      <div class="bg-blue-100 text-blue-900 px-4 py-2">
+      <div class="bg-blue-100 text-blue-900 px-4 py-2 mb-4">
         {{ session('status') }}
       </div>
     @endif
--- resources/views/_includes/_header.blade.php
+++ resources/views/_includes/_header.blade.php
@@ -1,57 +1,79 @@
 <header class="public">
   <div class="bg-gray-900 h-20">
     <div class="max-w-5xl px-4 mx-auto">
+
       <div class="flex justify-between items-start py-3">
+
         <a class="block text-4xl text-white w-40" href="{{ url('/') }}">
           {{ config('app.name') }}
         </a>
+
         <div class="hidden sm:flex justify-end items-end text-white">
-          <div id="category_btn" class="text-center cursor-pointer py-2 px-4">
-            <div>CATEGORY</div>
-            <div class="text-blue-400 text-xs">●</div>
+          @isset ($categories)
+            <div id="category_btn" class="text-center cursor-pointer py-2 px-4">
+              <div>CATEGORY</div>
+              <div class="text-blue-400 text-xs">●</div>
+            </div>
+          @endisset
+
+          @isset ($tags)
+            <div id="tag_btn" class="text-center cursor-pointer py-2 px-4">
+              <div>TAG</div>
+              <div class="text-blue-400 text-xs">●</div>
+            </div>
+          @endisset
           </div>
-          <div id="tag_btn" class="text-center cursor-pointer py-2 px-4">
-            <div>TAG</div>
-            <div class="text-blue-400 text-xs">●</div>
-          </div>
-        </div>
+
         <div class="sm:hidden header_nav_toggle dropdown_toggle ">
             <div class="header_nav_toggle_bar dropdown_toggle_bar block "></div>
         </div>
+
       </div>
     </div>
   </div>
-  <div id="category_menu" class="bg-gray-300 py-2 w-full hidden">
-    <div class="container mx-auto text-gray-700 flex flex-wrap justify-center">
-      @foreach ($categories as $category)
-        <a href="/category/{{ $category->id }}" class="block hover:text-gray-600 px-4 py-4 w-32">{{ $category->category_title}}</a>
-      @endforeach
-    </div>
-  </div>
-  <div id="tag_menu" class="bg-gray-300 py-2 w-full hidden">
-    <div class="container mx-auto text-gray-700 flex flex-wrap justify-center">
-      @foreach ($tags as $tag)
-        <a href="/tag/{{ $tag->id }}" class="block hover:text-gray-600 px-4 py-4 w-32">{{ $tag->tag_title}}</a>
-      @endforeach
-    </div>
-  </div>
 
-  <div class="dropdown_menu hidden bg-gray-300 py-2 w-full sm:static fixed">
-    <div class="container mx-auto">
-      <nav class="text-gray-700">
-        <div class="sm:hidden text-center py-4">CATEGORY</div>
-        <div class="flex flex-wrap justify-center">
-          @foreach ($categories as $category)
-            <a href="/category/{{ $category->id }}" class="block hover:text-gray-600 sm:border-0 border border-gray-500 rounded-full px-4 py-4 sm:w-32 w-auto sm:mb-0 mb-2 sm:mr-0 mr-2">{{ $category->category_title}}</a>
-          @endforeach
-        </div>
-        <div class="sm:hidden text-center py-4">TAG</div>
-        <div class="flex flex-wrap justify-center">
-          @foreach ($tags as $tag)
-                <a href="/tag/{{ $tag->id }}" class="block hover:text-gray-600 sm:border-0 border border-gray-500 rounded-full px-4 py-4 sm:w-32 w-auto sm:mb-0 mb-2 sm:mr-0 mr-2">{{ $tag->tag_title}}</a>
-          @endforeach
-        </div>
-      </nav>
+  @isset ($categories)
+    <div id="category_menu" class="bg-gray-300 py-2 w-full hidden">
+      <div class="container mx-auto text-gray-700 flex flex-wrap justify-center">
+        @foreach ($categories as $category)
+          <a href="/category/{{ $category->id }}" class="block hover:text-gray-600 px-4 py-4 w-32">{{ $category->category_title}}({{ $category->posts_count }})</a>
+        @endforeach
+      </div>
     </div>
-  </div>
+  @endisset
+
+  @isset ($tags)
+    <div id="tag_menu" class="bg-gray-300 py-2 w-full hidden">
+      <div class="container mx-auto text-gray-700 flex flex-wrap justify-center">
+        @foreach ($tags as $tag)
+          <a href="/tag/{{ $tag->id }}" class="block hover:text-gray-600 px-4 py-4 w-32">{{ $tag->tag_title}}({{ $tag->posts_count }})</a>
+        @endforeach
+      </div>
+    </div>
+  @endisset
+
+  @isset ($categories)
+    <div class="dropdown_menu hidden bg-gray-300 py-2 w-full sm:static fixed">
+      <div class="container mx-auto">
+        <nav class="text-gray-700">
+
+          <div class="sm:hidden text-center py-4">CATEGORY</div>
+          <div class="flex flex-wrap justify-center">
+            @foreach ($categories as $category)
+              <a href="/category/{{ $category->id }}" class="block hover:text-gray-600 sm:border-0 border border-gray-500 rounded-full px-4 py-4 sm:w-32 w-auto sm:mb-0 mb-2 sm:mr-0 mr-2">{{ $category->category_title}}({{ $category->posts_count }})</a>
+            @endforeach
+          </div>
+
+          <div class="sm:hidden text-center py-4">TAG</div>
+          <div class="flex flex-wrap justify-center">
+            @foreach ($tags as $tag)
+                  <a href="/tag/{{ $tag->id }}" class="block hover:text-gray-600 sm:border-0 border border-gray-500 rounded-full px-4 py-4 sm:w-32 w-auto sm:mb-0 mb-2 sm:mr-0 mr-2">{{ $tag->tag_title}}({{ $tag->posts_count }})</a>
+            @endforeach
+
+          </div>
+        </nav>
+      </div>
+    </div>
+  @endisset
+
 </header>
