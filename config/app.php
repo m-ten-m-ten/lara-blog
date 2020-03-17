c88b38481daf380b32a25d9f2bbf28e7c59f0bc5
tenmei matsuo <m.ten.m.ten@gmail.com>
2020-03-17 10:14:52 +0900

    app_urlをherokuに

--- config/app.php
+++ config/app.php
@@ -52,7 +52,8 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://laranote.herokuapp.com/'),
+    // 'url' => env('APP_URL', 'http://localhost'),
 
     'asset_url' => env('ASSET_URL', null),
 
