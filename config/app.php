d3af47c2e19096d027ac6f2d8556a2866b156ab3
tenmei matsuo <m.ten.m.ten@gmail.com>
2020-03-17 10:32:25 +0900

    app_url戻した

--- config/app.php
+++ config/app.php
@@ -52,8 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://laranote.herokuapp.com/'),
-    // 'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'http://localhost'),
 
     'asset_url' => env('ASSET_URL', null),
 
