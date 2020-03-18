489faa6e595e4f5eaf8b5db600b5760ad6de8ba6
tenmei matsuo <m.ten.m.ten@gmail.com>
2020-03-18 14:13:49 +0900

    ユーザー、管理者のパスワードリセット用メールテンプレートの作成・適用。

--- app/Notifications/AdminPasswordResetNotification.php
+++ app/Notifications/AdminPasswordResetNotification.php
@@ -9,7 +9,9 @@ class AdminPasswordResetNotification extends ResetPasswordNotification
 {
     public function toMail($notifiable)
     {
-        return (new MailMessage())
+        return (new MailMessage)->markdown('notifications.email')
+            ->subject('[' . config('app.name') . ']管理者パスワードのリセットリンク通知')
+            ->greeting('[' . config('app.name') . ']の管理者様')
             ->line('このメールは、パスワードリセットの申し込みをされた方に送信されています。')
             ->action('パスワードリセットページへ', url(config('app.url') . route('admin.reset', ['token' => $this->token, 'email' => $notifiable->getEmailForPasswordReset()], false)))
             ->line('もしパスワードリセットの申し込みをされていない場合、この通知は無視して下さい。');
