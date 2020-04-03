34ed3e720ac3b1efb666a9be47ee666619b38d8c
tenmei matsuo <m.ten.m.ten@gmail.com>
2020-04-03 14:42:31 +0900

    コード整形

--- routes/web.php
+++ routes/web.php
@@ -158,5 +158,5 @@ Route::prefix('contact')->namespace('Contact')->as('contact.')->group(function (
     Route::get('/confirm', 'ContactController@confirm')->name('confirm');
     Route::post('/confirm', 'ContactController@send');
     Route::get('/thanks', 'ContactController@thanks')->name('thanks');
-    });
+});
 Route::get('{post}', 'HomeController@post')->name('post');
