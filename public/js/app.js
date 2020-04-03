e5195ddc1fd882cff388c7eacd9e9cb7c4b46a7c
tenmei matsuo <m.ten.m.ten@gmail.com>
2020-04-03 17:29:17 +0900

    fixsidebar.jsがサイドバー出現時のみ有効化するよう変更。

--- public/js/app.js
+++ public/js/app.js
@@ -93331,32 +93331,36 @@ __webpack_require__.r(__webpack_exports__);
  * サイドバーで追従要素(#sidebar__fixed)の制御
  */
 function fixSidebar() {
-  var sidebar = $('#sidebar'),
-      sticked = $('#sidebar__fixed'),
-      main = $('#main'),
-      sidebarTop = sidebar.offset().top,
-      stickedOriginalTop = sticked.offset().top,
-      stickedHeight = sticked.height();
-  $(window).on('scroll resize', function () {
-    var scrollTop = $(window).scrollTop(),
-        contentBottom = main.offset().top + main.height();
+  var breakPoint = 1130; //sidebar出現ブレークポイント
 
-    if (scrollTop > stickedOriginalTop && scrollTop < contentBottom - stickedHeight) {
-      sticked.css({
-        'position': 'fixed',
-        'top': 0,
-        'width': sidebar.width()
-      });
-    } else if (scrollTop >= contentBottom - stickedHeight) {
-      sticked.css({
-        'position': 'absolute',
-        'top': contentBottom - stickedHeight,
-        'width': sidebar.width()
-      });
-    } else {
-      sticked.css('position', 'static');
-    }
-  });
+  if ($(window).width() > breakPoint) {
+    var sidebar = $('#sidebar'),
+        sticked = $('#sidebar__fixed'),
+        main = $('#main'),
+        sidebarTop = sidebar.offset().top,
+        stickedOriginalTop = sticked.offset().top,
+        stickedHeight = sticked.height();
+    $(window).on('scroll resize', function () {
+      var scrollTop = $(window).scrollTop(),
+          contentBottom = main.offset().top + main.height();
+
+      if (scrollTop > stickedOriginalTop && scrollTop < contentBottom - stickedHeight) {
+        sticked.css({
+          'position': 'fixed',
+          'top': 0,
+          'width': sidebar.width()
+        });
+      } else if (scrollTop >= contentBottom - stickedHeight) {
+        sticked.css({
+          'position': 'absolute',
+          'top': contentBottom - stickedHeight,
+          'width': sidebar.width()
+        });
+      } else {
+        sticked.css('position', 'static');
+      }
+    });
+  }
 }
 
 /***/ }),
--- resources/js/modules/fixSidebar.js
+++ resources/js/modules/fixSidebar.js
@@ -2,31 +2,36 @@
  * サイドバーで追従要素(#sidebar__fixed)の制御
  */
 export function fixSidebar() {
-  const sidebar = $('#sidebar'),
-        sticked = $('#sidebar__fixed'),
-        main = $('#main'),
-        sidebarTop = sidebar.offset().top,
-        stickedOriginalTop = sticked.offset().top,
-        stickedHeight = sticked.height();
 
-  $(window).on('scroll resize', function(){
-      let scrollTop = $(window).scrollTop(),
-          contentBottom = main.offset().top + main.height();
+  const breakPoint = 1130; //sidebar出現ブレークポイント
 
-      if ((scrollTop > stickedOriginalTop) && (scrollTop < contentBottom - stickedHeight)){
-          sticked.css({
-              'position': 'fixed',
-              'top': 0,
-              'width': sidebar.width()
-          });
-      } else if (scrollTop >= contentBottom - stickedHeight){
-          sticked.css({
-              'position': 'absolute',
-              'top': contentBottom - stickedHeight,
-              'width': sidebar.width()
-          });
-      } else {
-          sticked.css('position', 'static');
-      }
-  });
+  if($(window).width() > breakPoint){
+    const sidebar = $('#sidebar'),
+          sticked = $('#sidebar__fixed'),
+          main = $('#main'),
+          sidebarTop = sidebar.offset().top,
+          stickedOriginalTop = sticked.offset().top,
+          stickedHeight = sticked.height();
+
+    $(window).on('scroll resize', function(){
+        let scrollTop = $(window).scrollTop(),
+            contentBottom = main.offset().top + main.height();
+
+        if ((scrollTop > stickedOriginalTop) && (scrollTop < contentBottom - stickedHeight)){
+            sticked.css({
+                'position': 'fixed',
+                'top': 0,
+                'width': sidebar.width()
+            });
+        } else if (scrollTop >= contentBottom - stickedHeight){
+            sticked.css({
+                'position': 'absolute',
+                'top': contentBottom - stickedHeight,
+                'width': sidebar.width()
+            });
+        } else {
+            sticked.css('position', 'static');
+        }
+    });
+  }
 }
