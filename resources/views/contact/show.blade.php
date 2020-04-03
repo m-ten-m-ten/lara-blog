475ddb0837fc00468495b4d708b38306f53efc9b
tenmei matsuo <m.ten.m.ten@gmail.com>
2020-04-03 15:17:42 +0900

    エラー修正。

--- resources/views/contact/show.blade.php
+++ resources/views/contact/show.blade.php
@@ -7,18 +7,18 @@
   <ul>
     <li class="form__row">
       <label class="form__title">名前</label>
-      <input type="text" class="{{$errors->has('name') ? 'form__input-error' : 'form__input' }}" name="name" value="{{ old('name', $contact['name']) }}" required autocomplete="name" maxlength="50">
+      <input type="text" class="{{$errors->has('name') ? 'form__input-error' : 'form__input' }}" name="name" value="{{ old('name', $contact['name'])?: '' }}" required autocomplete="name" maxlength="50">
       @error('name')<span class="error-text">{{ $message }}</span>@enderror
     </li>
     <li class="form__row">
       <label class="form__title">メールアドレス</label>
       <span class="form__note">※返信が必要な方はご入力ください。</span>
-      <input type="email" class="{{$errors->has('email') ? 'form__input-error' : 'form__input' }}" name="email" value="{{ old('email', $contact['email']) }}" autocomplete="email">
+      <input type="email" class="{{$errors->has('email') ? 'form__input-error' : 'form__input' }}" name="email" value="{{ old('email', $contact['email'])?: '' }}" autocomplete="email">
       @error('email')<span class="error-text">{{ $message }}</span>@enderror
     </li>
     <li class="form__row">
       <label class="form__title">お問い合わせ内容</label>
-      <textarea class="{{$errors->has('body') ? 'form__input-error' : 'form__input' }}" name="body" type="text" rows="10" required maxlength="1000">{{ old('body', $contact['body']) }}</textarea>
+      <textarea class="{{$errors->has('body') ? 'form__input-error' : 'form__input' }}" name="body" type="text" rows="10" required maxlength="1000">{{ old('body', $contact['body'])?: '' }}</textarea>
       @error('body')<span class="error-text">{{ $message }}</span>@enderror
     </li>
   </ul>
